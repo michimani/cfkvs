82fb07e3e99705c6d6e408f2bc30081a09076875
michimani <michimani210@gmail.com>
2024-08-30 23:45:30 +0900

    feat: import from S3 #13

--- internal/commands/kvs.go
+++ internal/commands/kvs.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"context"
+	"errors"
 
 	"github.com/michimani/cfkvs/internal/output"
 	"github.com/michimani/cfkvs/libs"
@@ -15,8 +16,10 @@ type KvsCmd struct {
 type ListKvsSubCmd struct{}
 
 type CreateSubCmd struct {
-	Name    string `name:"name" help:"Name of the key value store." required:""`
-	Comment string `name:"comment" help:"Comment of the key value store."`
+	Name      string `name:"name" help:"Name of the key value store." required:""`
+	Comment   string `name:"comment" help:"Comment of the key value store."`
+	Bucket    string `name:"bucket" help:"S3 bucket name to import key value store, if you want."`
+	ObjectKey string `name:"object-key" help:"S3 object key to import key value store, if you want."`
 }
 
 func (c *ListKvsSubCmd) Run(globals *Globals) error {
@@ -43,8 +46,19 @@ func (c *CreateSubCmd) Run(globals *Globals) error {
 		return err
 	}
 
-	// TODO: import from S3
-	kvs, err := libs.CreateKvs[libs.KVSImportSourceS3](context.TODO(), cfc, c.Name, c.Comment, nil)
+	var srcS3 *libs.KVSImportSourceS3 = nil
+	if c.Bucket != "" {
+		if c.ObjectKey == "" {
+			return errors.New("object-key is required when bucket is specified")
+		}
+
+		srcS3 = &libs.KVSImportSourceS3{
+			Bucket: c.Bucket,
+			Key:    c.ObjectKey,
+		}
+	}
+
+	kvs, err := libs.CreateKvs(context.TODO(), cfc, c.Name, c.Comment, srcS3)
 	if err != nil {
 		return err
 	}
