17fed5b094911cfc0bcce51806db6c47e196c1f6
michimani <michimani210@gmail.com>
2024-09-15 12:27:52 +0900

    chore: add tests for libs.DescribeKeyValueStore #36

--- libs/cloudfront_test.go
+++ libs/cloudfront_test.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
 	cfTypes "github.com/aws/aws-sdk-go-v2/service/cloudfront/types"
+	kvs "github.com/aws/aws-sdk-go-v2/service/cloudfrontkeyvaluestore"
 	"github.com/michimani/cfkvs/libs"
+	"github.com/michimani/cfkvs/types"
 	"github.com/stretchr/testify/assert"
 	gomock "go.uber.org/mock/gomock"
 )
@@ -342,6 +345,156 @@ func Test_CreateKvs(t *testing.T) {
 
 			asst.NoError(err)
 			asst.NotNil(out)
+		})
+	}
+}
+
+func Test_DescribeKeyValueStore(t *testing.T) {
+	now := time.Now()
+	past := now.AddDate(0, -1, 0)
+
+	cases := []struct {
+		name   string
+		cfcOut struct {
+			Out   *cloudfront.DescribeKeyValueStoreOutput
+			Error error
+		}
+		kvscOut struct {
+			Out   *kvs.DescribeKeyValueStoreOutput
+			Error error
+		}
+		kvsName   string
+		wantError bool
+		expect    *types.KeyValueStoreFull
+	}{
+		{
+			name: "success",
+			cfcOut: struct {
+				Out   *cloudfront.DescribeKeyValueStoreOutput
+				Error error
+			}{
+				Out: &cloudfront.DescribeKeyValueStoreOutput{
+					KeyValueStore: &cfTypes.KeyValueStore{
+						Id:      aws.String("id"),
+						Name:    aws.String("kvs_name"),
+						Comment: aws.String("kvs_comment"),
+						Status:  aws.String("status"),
+						ARN:     aws.String("arn-for-kvs_name"),
+					},
+					ETag: aws.String("etag"),
+				},
+				Error: nil,
+			},
+			kvscOut: struct {
+				Out   *kvs.DescribeKeyValueStoreOutput
+				Error error
+			}{
+				Out: &kvs.DescribeKeyValueStoreOutput{
+					Created:          aws.Time(now),
+					LastModified:     aws.Time(past),
+					ItemCount:        aws.Int32(1),
+					TotalSizeInBytes: aws.Int64(1024),
+					FailureReason:    aws.String("failure_reason"),
+					KvsARN:           aws.String("arn-for-kvs_name"),
+					ETag:             aws.String("etag"),
+				},
+				Error: nil,
+			},
+			kvsName:   "kvs_name",
+			wantError: false,
+			expect: &types.KeyValueStoreFull{
+				ID:               "id",
+				ARN:              "arn-for-kvs_name",
+				Name:             "kvs_name",
+				Comment:          "kvs_comment",
+				Status:           "status",
+				ItemCount:        1,
+				TotalSizeInBytes: 1024,
+				Created:          now,
+				LastModified:     past,
+				FailureReason:    "failure_reason",
+				ETag:             "etag",
+			},
+		},
+		{
+			name: "failed to CloudFront:DescribeKeyValueStore",
+			cfcOut: struct {
+				Out   *cloudfront.DescribeKeyValueStoreOutput
+				Error error
+			}{
+				Out:   nil,
+				Error: errors.New("failed to describe key value store"),
+			},
+			kvscOut: struct {
+				Out   *kvs.DescribeKeyValueStoreOutput
+				Error error
+			}{
+				Out:   &kvs.DescribeKeyValueStoreOutput{},
+				Error: nil,
+			},
+			kvsName:   "kvs_name",
+			wantError: true,
+			expect:    nil,
+		},
+		{
+			name: "failed to CloudFrontKeyValueStore:DescribeKeyValueStore",
+			cfcOut: struct {
+				Out   *cloudfront.DescribeKeyValueStoreOutput
+				Error error
+			}{
+				Out: &cloudfront.DescribeKeyValueStoreOutput{
+					KeyValueStore: &cfTypes.KeyValueStore{
+						ARN: aws.String("arn-for-kvs_name"),
+					},
+				},
+				Error: nil,
+			},
+			kvscOut: struct {
+				Out   *kvs.DescribeKeyValueStoreOutput
+				Error error
+			}{
+				Out:   nil,
+				Error: errors.New("failed to describe key value store"),
+			},
+			kvsName:   "kvs_name",
+			wantError: true,
+			expect:    nil,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			asst := assert.New(tt)
+			ctx := context.Background()
+
+			ctrl := gomock.NewController(tt)
+			mcfc := libs.NewMockCloudFrontClient(ctrl)
+			mcfc.EXPECT().
+				DescribeKeyValueStore(ctx, &cloudfront.DescribeKeyValueStoreInput{
+					Name: &c.kvsName,
+				}).
+				Return(c.cfcOut.Out, c.cfcOut.Error)
+
+			mkvsc := libs.NewMockCloudFrontKeyValueStoreClient(ctrl)
+
+			if c.cfcOut.Error == nil {
+				mkvsc.EXPECT().
+					DescribeKeyValueStore(ctx, &kvs.DescribeKeyValueStoreInput{
+						KvsARN: aws.String("arn-for-" + c.kvsName),
+					}).
+					Return(c.kvscOut.Out, c.kvscOut.Error)
+			}
+
+			out, err := libs.DescribeKeyValueStore(ctx, mcfc, mkvsc, c.kvsName)
+			if c.wantError {
+				asst.Error(err)
+				asst.Nil(out)
+				return
+			}
+
+			asst.NoError(err)
+			asst.NotNil(out)
+			asst.Equal(c.expect, out)
 		})
 	}
 }
